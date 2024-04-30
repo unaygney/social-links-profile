653463a7a1c7549cd281ba9ee3750e3d5e2c859d
Guney Unay <guneymarketing17@hotmail.com>
2024-04-30 20:55:52 +0300

    :lipstick: UI changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main>test</main>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+    </div>
+  );
 }
